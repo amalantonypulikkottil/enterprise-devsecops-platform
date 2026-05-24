fa9e1c0bfd7b83a489d9ba3be4a524cf7fc256e1
Amal Antony <amalantony200200@gmail.com>
2026-05-24 10:12:47 +0530

    Initial commit

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Enterprise DevSecOps Pipeline Running Successfully 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Application running on port 3000");
+});
